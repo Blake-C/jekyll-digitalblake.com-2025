91bc24de514e38c91502186f890f412c13cf3829
Blake Cerecero <Blake@DigitalBlake.com>
2026-04-17 21:08:43 -0500

    Fixed inlined critical CSS sourcemap and font URL 404s in dev mode

--- script/rewrite-critical-urls.mjs
+++ script/rewrite-critical-urls.mjs
@@ -2,10 +2,13 @@
 import fs from 'node:fs'
 
 const target = '_includes/critical.min.css'
+const map = `${target}.map`
 const source = fs.readFileSync(target, 'utf8')
 
 const rewritten = source
 	.replace(/url\((['"]?)\.\.\/fonts\/([^'")]+)\1\)/g, 'url({{ "/assets/fonts/$2" | relative_url }})')
 	.replace(/url\((['"]?)\.\.\/images\/([^'")]+)\1\)/g, 'url({{ "/assets/images/$2" | relative_url }})')
+	.replace(/\s*\/\*#\s*sourceMappingURL=[^*]*\*\/\s*$/g, '')
 
 fs.writeFileSync(target, rewritten)
+fs.rmSync(map, { force: true })
